f45a43487a6ea853619e38af1d8a18385ad96e7b
Melina Bueno <melinabueno123@gmail.com>
2025-08-28 10:20:43 -0300

    Add endpoint to create multiple types of bank accounts

---
+++ src/Core/Entities/LineOfCreditAccount.cs
@@ -0,0 +1,23 @@
+namespace Core.Entities
+{
+    public class LineOfCreditAccount : BankAccount
+    {
+        public LineOfCreditAccount(string name, decimal initialBalance, decimal creditLimit)
+            : base(name, initialBalance, -creditLimit)  
+        {
+        }
+
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance < 0)
+            {
+                decimal interest = -Balance * 0.07m;
+                MakeWithdrawal(interest, DateTime.Now, "Charge monthly interest");
+            }
+        }
+        protected override Transaction? CheckWithdrawalLimit(bool isOverdrawn) =>
+            isOverdrawn
+            ? new Transaction(-20, DateTime.Now, "Apply overdraft fee")
+            : default;
+    }
+}

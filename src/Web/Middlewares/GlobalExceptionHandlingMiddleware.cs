56fb17c51bf4dac74560735dc5947b324d679d02
efalabrini <edfsoft@gmail.com>
2025-10-08 12:51:11 -0300

    Implement json response in GlobalExceptionHandlingMiddleware

--- src/Web/Middlewares/GlobalExceptionHandlingMiddleware.cs
+++ src/Web/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -1,5 +1,8 @@
+using System.Net;
+using System.Text.Json;
 using Core.Exceptions;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Middleware;
 
@@ -21,13 +24,45 @@ public class GlobalExceptionHandlingMiddleware : IMiddleware
         }
         catch (AppValidationException ex)
         {
-            context.Response.StatusCode = 400;
-            await context.Response.WriteAsync($"¡Oops! Algo pasó: {ex.Message}");
+            _logger.LogError(ex, ex.Message);
+
+            int statusCode = (int)HttpStatusCode.BadRequest;
+
+            ProblemDetails problem = new()
+            {
+                Status = statusCode,
+                Type = "https://bankaccountapi/errors/appvalidation",
+                Title = "Validation error",
+                Detail = ex.Message
+            };
+
+            string json = JsonSerializer.Serialize(problem);
+
+            context.Response.ContentType = "application/json";
+            
+            await context.Response.WriteAsync(json);
         }
         catch (Exception ex)
         {
-            context.Response.StatusCode = 500;
-            await context.Response.WriteAsync($"¡Oops! Error interno del servidor: {ex.Message}");
+            _logger.LogError(ex, ex.Message);
+
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+
+            context.Response.StatusCode = statusCode;
+
+            ProblemDetails problem = new()
+            {
+                Status = statusCode,
+                Type = "Server error",
+                Title = "Server error",
+                Detail = "An internal server"
+            };
+
+            string json = JsonSerializer.Serialize(problem);
+
+            context.Response.ContentType = "application/json";
+
+            await context.Response.WriteAsync(json);
         }
     }
 }

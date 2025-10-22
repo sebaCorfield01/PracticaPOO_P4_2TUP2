a5218cf7b0e01383d80302f60764cf73841d0761
efalabrini <edfsoft@gmail.com>
2025-10-22 13:01:19 -0300

    Add UserService, update IUserRepository

--- src/Core/Interfaces/IUserRepository.cs
+++ src/Core/Interfaces/IUserRepository.cs
@@ -7,4 +7,16 @@ public interface IUserRepository
 {
     User? GetUserByUserName(string userName);
 
+    User? GetById(int id);
+
+    List<User> List();
+
+    User Add(User entity);
+
+    void Update(User entity);
+
+    void Delete(User entity);
+
+    int SaveChanges();
+
 }
